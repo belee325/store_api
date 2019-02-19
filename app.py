04feb50cfc6cda2b6614485dde423a099d267754
belee325 <belee325@gmail.com>
2019-02-19 15:48:13 -0500

    fixed

--- app.py
+++ app.py
@@ -28,4 +28,8 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
 
-    app.run(debug=True, host='0.0.0.0')
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
+    app.run(debug=True)
