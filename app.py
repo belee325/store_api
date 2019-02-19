644a47d62c73ba06d219e26f6250ec99ce42e399
belee325 <belee325@gmail.com>
2019-02-19 15:13:01 -0500

    importing db correctly

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 api = Api(app)
 jwt = JWT(app, authenticate, identify)  # /auth
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
